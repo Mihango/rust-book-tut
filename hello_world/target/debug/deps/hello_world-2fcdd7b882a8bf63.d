7692e04cca22c9eeda354e13894c96d5b1a5bee6
mihango <kennedy.kariuki@toq.io>
2022-10-21 08:40:28 +0300

    ignore target folder

--- hello_world/target/debug/deps/hello_world-2fcdd7b882a8bf63.d
+++
@@ -1,5 +0,0 @@
-/Users/mihango/Projects/samples/rust/rust-book/hello_world/target/debug/deps/hello_world-2fcdd7b882a8bf63.rmeta: src/main.rs
-
-/Users/mihango/Projects/samples/rust/rust-book/hello_world/target/debug/deps/hello_world-2fcdd7b882a8bf63.d: src/main.rs
-
-src/main.rs:
--- hello_world/target/debug/deps/hello_world-42294629c6e37702.d
+++
@@ -1,5 +0,0 @@
-/Users/mihango/Projects/samples/rust/rust-book/hello_world/target/debug/deps/hello_world-42294629c6e37702.rmeta: src/main.rs
-
-/Users/mihango/Projects/samples/rust/rust-book/hello_world/target/debug/deps/hello_world-42294629c6e37702.d: src/main.rs
-
-src/main.rs:
