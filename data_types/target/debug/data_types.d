d5eadb1fc94a1ce38b2bf73262aa449977917e25
mihango <kennedy.kariuki@toq.io>
2022-10-22 13:29:33 +0300

    arrange files accordingly with book sections

--- data_types/target/debug/data_types.d
+++
@@ -1,1 +0,0 @@
-/Users/mihango/Projects/samples/rust/rust-book/data_types/target/debug/data_types: /Users/mihango/Projects/samples/rust/rust-book/data_types/src/main.rs
--- hello_world/target/debug/deps/hello_world-93bf6afec42167f4.d
+++
@@ -1,5 +0,0 @@
-/Users/mihango/Projects/samples/rust/rust-book/hello_world/target/debug/deps/hello_world-93bf6afec42167f4: src/main.rs
-
-/Users/mihango/Projects/samples/rust/rust-book/hello_world/target/debug/deps/hello_world-93bf6afec42167f4.d: src/main.rs
-
-src/main.rs:
