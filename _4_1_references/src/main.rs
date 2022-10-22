d333442ebe3d21ea0f805cf53ba0a954bd1c9ff6
mihango <kennedy.kariuki@toq.io>
2022-10-22 16:43:59 +0300

    added slice description

--- _4_1_references/src/main.rs
+++ _4_1_references/src/main.rs
@@ -26,6 +26,8 @@ fn caluculate_length(s: String) -> (String, usize) {
 fn reference_sample() {
     println!("\n>>>>>>>>>>> reference >>>>>>>>>>>");
     let s = String::from("Hello"); // s into scope
+
+    // the action of creating a reference is call borowwing in rust i.e have borrowed s below
     let len = caluculate_length_ref(&s); // ownership is not move and use a reference/pointer which returns usize
     // s is valid here
     println!("Length of {s} is: {len}");
@@ -34,4 +36,15 @@ fn reference_sample() {
 
 fn caluculate_length_ref(s: &String) -> usize {
     s.len()
-}
+} // s goes out of scope but since it does not own the value referenced, it is not dropped
+
+// Rules of reference
+//      1. You can only have one mutable reference or several immutable reference -- to have them their lifetimes must complete 
+//          the mutable
+//      2. Reference must always be valid -- you can not reference a value that has already been dropped
+
+// &str string slice eg string literal can be used to reference and a string or an array and ensure that value is not 
+// dropped when it is being reference -- eg when getting first word separated by space in a string, return the index of it, and
+// then clearing the string my bring problem if we try to use it when we have cleared the string
+// to avoid this scenario - use string slices which will ensure that clearing os s does not happend as it does not allow mutable
+// borrow when you have an immutable reference
